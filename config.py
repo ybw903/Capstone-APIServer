1e7f5f8d191bea15b5891d541c3f380f3527729e
병우 유 <conner903@naver.com>
2020-11-29 13:44:03 +0900

    fixed untracked files

--- config.py
+++
@@ -1,3 +0,0 @@
-config = {'MYSQL_DATABASE_USER':'admin','MYSQL_DATABASE_PASSWORD':'real1735','MYSQL_DATABASE_DB':'DB',
-          'MYSQL_DATABASE_HOST':'capstone.cihbidb6hske.ap-northeast-2.rds.amazonaws.com','JWT_SECRET_KEY':'secret'}
-FCM_KEY = 'AAAAYMumGGg:APA91bGeULLBT6jcIn6CSPl_jV1iSyACvXBZexMWtvIvzeWAyPwWxkjmZ-48FIZETDQVP48dlhkz8-s6UAVHbnSQ2cLzNxAKeUM--IdptYL-m96FkTG5tFicM4wyORgVsb0wAJPCvA4X'
